713c3b7f16624c578328cd76bdb5b4ba48dda633
Kenny McCormick <kennyrmccormick+github@gmail.com>
2022-10-22 19:10:34 -0500

    add basic game files

---
+++ scripts/states/MainMenuState.js
@@ -0,0 +1,50 @@
+import { State } from "./State.js";
+
+import * as PageUtility from "../PageUtility.js";
+
+import { PlayState } from "./PlayState.js";
+import { SettingsState } from "./SettingsState.js";
+
+export class MainMenuState extends State
+{
+    init(stateMachine)
+    {
+        this.stateMachine = stateMachine;
+
+        PageUtility.addStyle("mainMenu");
+
+        this.mainMenu = document.createElement("div");
+        this.mainMenu.id = "mainMenu";
+        this.mainMenu.classList = "display-flex justify-center align-center flex-column flex-gap";
+        document.body.appendChild(this.mainMenu);
+
+        const button = document.createElement("button");
+        button.id = "playGame";
+        button.textContent = "Play Game";
+        button.addEventListener("click", (event) =>
+        {
+            this.stateMachine.changeState(new PlayState());
+        });
+        this.mainMenu.appendChild(button);
+
+        const settingsButton = document.createElement("button");
+        settingsButton.id = "settingsButton";
+        settingsButton.textContent = "Settings";
+        settingsButton.addEventListener("click", (event) =>
+        {
+            this.stateMachine.pushState(new SettingsState());
+        });
+        this.mainMenu.appendChild(settingsButton);
+
+        console.log("MainMenuState ready.");
+    }
+
+    cleanup()
+    {
+        PageUtility.removeStyle("mainMenu");
+
+        this.mainMenu.remove();
+
+        console.log("MainMenuState cleaned up.");
+    }
+};
