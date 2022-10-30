1073cb96b7115d3eac21695321f197bf63dc7217
Kenny McCormick <kennyrkun@users.noreply.github.com>
2022-10-30 15:03:32 -0500

    remvoe money and nose from player

--- scripts/Player.js
+++ scripts/Player.js
@@ -13,15 +13,6 @@ export class Player extends ItemCarrier
         
         super(geometry, material);
 
-        const nose = GeometryUtil.createScaledCube(0.4, 1, 0.2, 0x0000aa);
-        nose.position.z = 0.8;
-        nose.position.y = 0.5;
-        this.add(nose);
-        
-        this.money = 0;
-        
-        this.carriedMoney = new Array();
-
         //this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 200);
 		        
         this.maxSpeed = 0.3;
@@ -30,17 +21,5 @@ export class Player extends ItemCarrier
     update(deltaTime)
     {
         super.update(deltaTime);
-
-        for (const money of this.carriedMoney)
-        {
-            if (money.elapsedTime > money.moveTime)
-            {
-                scene.remove(money);
-                this.carriedMoney.splice(this.carriedMoney.indexOf(money), 1);
-                continue;
-            }
-            
-            money.updateTarget(this.position, new Vector3(0, 0, 0.5));
-        }
     }
 };
