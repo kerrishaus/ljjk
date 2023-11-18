38ccef1257d464f04a7d57af06df47c798f4f00b
kennyrkun <kennyrkun@kerrishaus.com>
2023-11-17 23:23:47 -0600

    include mathutility in projectile and delete player.lastAttacker when they die

--- scripts/Projectile.js
+++ scripts/Projectile.js
@@ -3,6 +3,7 @@ import { Plane, Raycaster, SphereGeometry, Mesh, MeshPhongMaterial, Vector2, Rep
 import { Player } from "./Player.js";
 import { Actor } from "./Actor.js";
 import { Enemy } from "./Enemy.js";
+import * as MathUtility from "./MathUtility.js";
 
 export class Projectile extends Actor
 {
@@ -42,7 +43,11 @@ export class Projectile extends Actor
                     let enemy = new Enemy(player);
                     enemy.position.copy(new Vector3(MathUtility.getRandomInt(-15, 15), MathUtility.getRandomInt(-15, 15), 0));
                     scene.add(enemy);
+
+                    if (player.lastAttacker = this.target)
+                        player.lastAttacker = null;
                 }
+
                 scene.remove(this.target);
             }
 
